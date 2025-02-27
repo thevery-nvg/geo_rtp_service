25bbbfb0f9732217080bf8b8bd21330e6a833eea
box7 <theverynvg@vk.com>
2025-02-27 20:49:37 +0400

    Переработан эндпойнт загрузки файлов, добавлено логгирование и создание папки юзера и даты

--- src/cloud/uploader.py
+++ src/cloud/uploader.py
@@ -1,20 +1,23 @@
+from datetime import datetime
 from fastapi import APIRouter, UploadFile, File, Form, Request, Depends
 import shutil
 from typing import List
-
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, FileResponse
 from pathlib import Path
 from auth import current_user_strict
 from auth.models import User
+from core.config import settings
+from loguru import logger
+from services import format_size
 
 upload_router = APIRouter(
     tags=["Upload"])
 
 upload_router.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
-cloud=Path.cwd().joinpath("cloud")
+cloud = Path(settings.cloud.url)
 
 
 @upload_router.get("/upload", response_class=HTMLResponse)
@@ -23,8 +26,15 @@ async def upload_page(request: Request):
 
 
 @upload_router.post("/upload")
-async def upload_images(files: List[UploadFile] = File(...)):
+async def upload_files(files: List[UploadFile] = File(...), user="Aleshka"):
+    logger.info(f"{user} is uploading {len(files)} files; size = "
+                f"{format_size(sum([file.size for file in files]))};{[file.filename for file in files]}")
+    if not cloud.joinpath(user).exists():
+        cloud.joinpath(user).mkdir()
+    date = str(datetime.now().date())
+    if not cloud.joinpath(user).joinpath(date).exists():
+        cloud.joinpath(user).joinpath(date).mkdir()
     for file in files:
-        with open(cloud.joinpath(file.filename), 'wb') as buffer:
+        with open(cloud.joinpath(user).joinpath(date).joinpath(file.filename), 'wb') as buffer:
             shutil.copyfileobj(file.file, buffer)
     return {"filename": [file.filename for file in files]}
